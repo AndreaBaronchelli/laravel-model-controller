d34ee7a6b1141e9ebf959442f6d4fcf7c50b6d95
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-06-09 15:06:18 +0200

    add main tasks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>main here</div>
+    <div class="cards">
+        @foreach ($movies as $movie)
+            <div class="card">
+                <h4>Titolo: {{$movie->title}}</h4>
+                <div class="original-title">Titolo originale: {{$movie->original_title}}</div>
+                <div class="nationality">Nazione: {{$movie->nationality}}</div>
+                <div class="date">Data di uscita: {{$movie->date}}</div>
+                <div class="vote"><strong>Voto: {{$movie->vote}}</strong></div>
+            </div>
+        @endforeach
+    </div>
 @endsection
